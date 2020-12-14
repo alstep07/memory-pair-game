9be06748a284dadef5c24f127a7bb97712769fd4
Alexandr Stepanenko <alexstep07@icloud.com>
2020-12-15 01:49:47 +0200

    add turnCards

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const main = document.querySelector(".main");
 const frontImgSrc = "./img/front.png";
 const ids = [0,1,2,3,4,5];
+let openCards = [];
+
+startNewGame();
+
+function closeCards (arr) {
+	arr.forEach(card => card.classList.toggle('card-active'));
+	arr.length = 0;
+}
 
 function getRandomIds(arr){
 	return [...arr,...arr].sort(function(){return 0.5 - Math.random()});
 }
 
-function tryMatch(){
-	
+function turnCards(arr){
+	if (arr.length === 2) {
+		if (arr[0].id === arr[1].id) {
+			arr.forEach(item => item.style.visibility="hidden");
+			arr.length = 0;
+		} else {
+			setTimeout(closeCards(arr), 1000);
+		}
+	}
 }
+
 
 function createCard(id) {
 	const card = document.createElement("div");
@@ -23,6 +39,7 @@ function createCard(id) {
 
 	frontImg.setAttribute("src", frontImgSrc);
 	backImg.setAttribute("src", `./img/${id}.png`);
+	card.setAttribute("id", id);
 
 	front.classList.add("card__front");
 	back.classList.add("card__back");
@@ -32,9 +49,13 @@ function createCard(id) {
 
 	card.addEventListener("click", function () {
 		card.classList.toggle("card-active");
+		openCards.push(card);
+		turnCards(openCards);
 	});
 
 	main.append(card);
 }
 
-getRandomIds(ids).forEach(id => createCard(id));
+function startNewGame(){
+	getRandomIds(ids).forEach(id => createCard(id));
+}
